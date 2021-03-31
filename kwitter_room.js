114f151699a896a5aef7b8f0217d6afeb836d0b7
Gourang-dev <77156496+Gourang-dev@users.noreply.github.com>
2021-03-31 19:05:33 +0530

    Update kwitter_room.js

--- kwitter_room.js
+++ kwitter_room.js
@@ -52,5 +52,5 @@ function redirectToRoomName(name) {
 function logout() {
     localStorage.removeItem("user_name");
     localStorage.removeItem("room_name");
-    window.location = "kwitter.html";
+    window.location = "index.html";
 }
